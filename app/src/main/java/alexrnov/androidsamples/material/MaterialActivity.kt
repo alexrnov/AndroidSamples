331259e455852f40cbf79fcb7571a9df0dd4dbc1
alexrnov <a.novopashin5@gmail.com>
2020-01-27 16:57:50 +0900

    material design

--- app/src/main/java/alexrnov/androidsamples/material/MaterialActivity.kt
+++ app/src/main/java/alexrnov/androidsamples/material/MaterialActivity.kt
@@ -6,11 +6,27 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import alexrnov.androidsamples.Initialization.TAG
+import android.widget.Button
 
 class MaterialActivity: AppCompatActivity() {
+  private var button5: Button? = null
+
   override fun onCreate(bundle: Bundle?) {
     super.onCreate(bundle)
     setContentView(R.layout.activity_material)
+    button5 = findViewById(R.id.material_button_5)
+
+    /*
+     * java: button5.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+      }
+    });
+     *
+     */
+    button5?.setOnClickListener {
+      Log.i(TAG, "button5 click")
+    }
   }
 
   fun button1(view: View) {
