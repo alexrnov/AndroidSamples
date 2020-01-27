c966053fd21cf0e989a377df3254ecf2bf33aba6
alexrnov <a.novopashin5@gmail.com>
2020-01-27 10:13:26 +0900

    material design

--- app/src/main/java/alexrnov/androidsamples/material/MaterialActivity.kt
+++ app/src/main/java/alexrnov/androidsamples/material/MaterialActivity.kt
@@ -2,11 +2,18 @@ package alexrnov.androidsamples.material
 
 import alexrnov.androidsamples.R
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import alexrnov.androidsamples.Initialization.TAG
 
 class MaterialActivity: AppCompatActivity() {
   override fun onCreate(bundle: Bundle?) {
     super.onCreate(bundle)
     setContentView(R.layout.activity_material)
+  }
+
+  fun button1(view: View) {
+    Log.i(TAG, "button1 click")
   }
 }
