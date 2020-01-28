0574ee7e48cd5934447a4065d4ff5731f1221455
alexrnov <a.novopashin5@gmail.com>
2020-01-28 11:53:07 +0900

    material design

--- app/src/main/java/alexrnov/androidsamples/material/MaterialActivity2.kt
+++ app/src/main/java/alexrnov/androidsamples/material/MaterialActivity2.kt
@@ -11,9 +11,8 @@ import android.content.SharedPreferences
 import android.widget.Button
 
 /**
- * Демо кнопок material design
  * Для кнопок material design следует переопределять метод onClick программно,
- * поскольку метод onClick определенный в лэйауте может не работать.
+ * поскольку метод onClick, определенный в лэйауте, может не работать.
  */
 class MaterialActivity2: AppCompatActivity() {
 
